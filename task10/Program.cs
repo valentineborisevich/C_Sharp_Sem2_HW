f360c165c2dadef03cb8253f7556e971d47fe838
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-21 14:14:35 +0300

    tasks 10 finished

--- task10/Program.cs
+++ task10/Program.cs
@@ -3,3 +3,8 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Your Random 3 gigits Number " + number);
+int newnumber = number % 100;
+int middledigit = newnumber / 10;
+Console.WriteLine("Middle digit is " + middledigit);
