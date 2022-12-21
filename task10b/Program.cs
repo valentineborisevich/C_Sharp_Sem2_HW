9d73e2fe73cfdbd9926e022aaa85550488127f15
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-21 14:25:55 +0300

    tasks 10b finished

--- task10b/Program.cs
+++ task10b/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Enter three digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 && number < 1000)
+{
+    int newnumber = number % 100;
+    int middledigit = newnumber / 10;
+    Console.WriteLine("Middle digit is " + middledigit);
+}
+else
+{
+    Console.WriteLine("Enter three digit number");
+}
