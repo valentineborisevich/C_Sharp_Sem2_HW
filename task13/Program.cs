7e936f0e0536bff0cc14450a74af8a69882b4df3
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-24 12:56:00 +0300

    task 15 finished

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,5 +5,9 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
-Console.WriteLine("Enter any number");
+Console.WriteLine("Enter three digit number");
 int number = Convert.ToInt32(Console.ReadLine());
+int newnumber = number % 100;
+int thirddigit = newnumber % 10;
+Console.WriteLine("Third digit is " + thirddigit);
+
--- task15/Program.cs
+++ task15/Program.cs
@@ -3,3 +3,17 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine("Enter number, which idicates the day of the week.");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number <= 5 && number > 0)
+{
+    Console.WriteLine("This is workday, pal.");
+}
+else if(number <= 7 && number > 5)
+{
+    Console.WriteLine("Welcome to the weekend.");
+}
+else
+{
+    Console.WriteLine("Try again.");
+}
