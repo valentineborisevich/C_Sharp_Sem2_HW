c36ba339c5547a674ead84787fe3c548623a92c0
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-27 14:29:58 +0300

    task 13 finished

--- task13/Program.cs
+++ task13/Program.cs
@@ -5,9 +5,16 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
-Console.WriteLine("Enter three digit number");
-int number = Convert.ToInt32(Console.ReadLine());
-int newnumber = number % 100;
-int thirddigit = newnumber % 10;
-Console.WriteLine("Third digit is " + thirddigit);
+Console.WriteLine("Enter any number. I will return third digit");
+string number = Console.ReadLine();
+char[] contents = number.ToCharArray();
+int n = contents.Length;
+if(n > 2 && Char.IsDigit (contents[2]))
+{
+    Console.WriteLine(contents[2]);
+}
+else
+{
+    Console.WriteLine("Try again. There's no third digit");
+}
 
